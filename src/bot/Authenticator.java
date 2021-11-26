10a359c118c17c8c8ac0b0c107d0b5807f034956
lorndor01 <lorndor01@gmail.com>
2021-11-25 22:41:57 -0500

    added gui popup for OAUTH

--- src/bot/Authenticator.java
+++ src/bot/Authenticator.java
@@ -9,19 +9,37 @@ import java.net.URISyntaxException;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
+import twittergui.SceneBuilder;
 
 
 
 
 public class Authenticator {
-	public void authenticate(Twitter twitter) {
+	@FXML
+	private Button submitButton;
+	@FXML
+	private TextField PINTextBox;
+	public void authenticate(Twitter twitter, Stage stage) {
 		//pull the key and secet from preferences
 		Preferences prefs = Preferences.userRoot().node(this.getClass().getName());
-
+		try {
+			prefs.clear();
+		} catch (BackingStoreException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		//If user hasn't been authenticated then key & secret will be null.
 		String key = prefs.get("key", null);
 		String secret = prefs.get("secret", null);
@@ -31,9 +49,9 @@ public class Authenticator {
 		if(key!=null && secret != null) {
 			//User has already been authenticated
 			accessToken = new AccessToken(key, secret);
-			twitter.setOAuthAccessToken(accessToken);
+			twitter.setOAuthAccessToken(accessToken); 
 		}
-
+ 
 		/*
 		 * By now we either have a completed OAuth sequence and null AccessToken OR
 		 * we have a valid non-null AccessToken
@@ -41,8 +59,20 @@ public class Authenticator {
 		try {
 			//If request token is already avaiable then an IllegalStateException will be thrown
 			RequestToken requestToken = twitter.getOAuthRequestToken();
+			SceneBuilder sb = new SceneBuilder();
+			Scene scene = sb.getOAuthScene(twitter, requestToken, prefs, stage);
+			stage.setScene(scene);
+			stage.show();
 			if(accessToken == null) {
-				accessToken = getAccessToken(twitter, requestToken);
+				try {
+					Desktop.getDesktop().browse(new URI(requestToken.getAuthenticationURL()));
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (URISyntaxException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			}
 		}
 		catch(IllegalStateException e) {
@@ -56,49 +86,6 @@ public class Authenticator {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		prefs.put("key", accessToken.getToken());
-		prefs.put("secret", accessToken.getTokenSecret());
 	}
 	
-	/*
-	 * This method only gets called if the user does not already have an access token
-	 */
-	private AccessToken getAccessToken(Twitter twitter, RequestToken requestToken) {
-		AccessToken accessToken = null;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while(accessToken == null) {
-			
-			System.out.println(requestToken.getAuthorizationURL());
-			System.out.println("Enter the PIN (if available) and hit enter after you have granted access. [PIN] ");
-			try {
-				Desktop.getDesktop().browse(new URI(requestToken.getAuthenticationURL()));
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (URISyntaxException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			try {
-				String PIN = br.readLine();
-				if(PIN.length()>0) {
-					accessToken = twitter.getOAuthAccessToken(requestToken, PIN);
-				}
-				else {
-					accessToken = twitter.getOAuthAccessToken(requestToken);
-				}
-				return accessToken;
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				System.out.println("Failed to read PIN...");
-			} catch (TwitterException te) {
-				if (401 == te.getStatusCode()) {
-                    System.out.println("Unable to get the access token.");
-                } else {
-                    te.printStackTrace();
-                }
-			}
-		}
-		return null;
-	}
 }
